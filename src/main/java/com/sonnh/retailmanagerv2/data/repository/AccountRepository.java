36fae86c22132b06dbbe18b738152b7694505158
meoupa1999 <57126326+meoupa1999@users.noreply.github.com>
2025-10-29 13:31:34 +0700

    add UserDetails and MyUserDetailsService

--- src/main/java/com/sonnh/retailmanagerv2/data/repository/AccountRepository.java
+++ src/main/java/com/sonnh/retailmanagerv2/data/repository/AccountRepository.java
@@ -1,10 +1,15 @@
 package com.sonnh.retailmanagerv2.data.repository;
 
 import com.sonnh.retailmanagerv2.data.domain.Account;
+import com.sonnh.retailmanagerv2.data.domain.Warehouse_WarehouseInventory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 @Repository
 public interface AccountRepository extends JpaRepository<Account, UUID> {
+    @Query("SELECT a FROM Account a WHERE a.username = :username")
+    Optional<Account>  findByUsername(String username);
 }
